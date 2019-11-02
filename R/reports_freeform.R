684ce4ffb726e7f940db9a0096bb7bb782f5dbf0
Ben Woodard <benrwoodard@gmail.com>
2019-11-02 16:52:31 -0500

    edited the example function arguments

--- R/reports_freeform.R
+++ R/reports_freeform.R
@@ -1,24 +1,36 @@
 #' Get a ranked reports
 #'
+#' Organizes the arguments into a json string and then structures the data after the internal function makes
+#' the api call. Only runs a single dimension with as many metrics as you want.
+#'
 #' @param date_range A two length vector of start and end Date objects
 #' @param metrics Metric to send
 #' @param dimensions Dimension to send
-#' @param num_rows How many rows
-#' @param rsid Adobe report number
+#' @param top How many rows. Defualt is set to 50
+#' @param metricSort Presorts the table by metrics. Values are either 'asc' or 'desc'.
+#' @param metricFilter Was originally going to be used for creating multi dimension json strings but not sure if this is needed. Leaving it here just in case.
+#' @param dimensionSort Presorts the table by dimension.  Not as helpful as metricSort but still has some use.  If metricSort is 'False' the dimension sort will be used.
+#' @param rsid Adobe report suite id number. Taken from the global environment by default if not provided.
 #'
-
+#' @return Data Frame
 #' @export
 #' @import assertthat httr tidyverse
 #'
-aa_ranked_report <- function(date_range,
-                              metrics,
-                              dimensions,
-                              top = 50,
-                              metricSort = FALSE,
-                              metricFilters = NULL,
-                              dimensionSort = 'asc',
-                              rsid = Sys.getenv("AA_RSID")){
-
+#' @example
+#' \dontrun{
+#' report <- aa_ranked_report(date_range= c('2019-01-01','2019-01-31'),
+#'                            metrics = c("visits","pageviews"),
+#'                            dimensions = "page")
+#' }
+#'
+aa_ranked_report <- function(rsid = Sys.getenv("AA_REPORTSUITE_ID"),
+                             date_range,
+                             metrics,
+                             dimensions,
+                             top = 50,
+                             metricSort = FALSE,
+                             metricFilters = NULL,
+                             dimensionSort = 'asc') {
 
   # set the timeframe variable
   timeframe <- make_timeframe(date_range[[1]], date_range[[2]])
