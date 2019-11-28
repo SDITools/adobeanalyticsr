3aa88ce7ed3f96e1c61d40e83c2f63baeb133786
Ben Woodard <benrwoodard@gmail.com>
2019-11-28 06:02:33 -0500

    Fixed the aa_get_metrics request by adding the correct global variable, trimming the expansion variable whitespaces that may appear and removing the "c()" function as this simply needs to be a list not a seperate url for each expansion value.

--- R/get_metrics.R
+++ R/get_metrics.R
@@ -6,15 +6,17 @@
 #' @param expansion options  c('tags', 'allowedForReporting', 'categories'))
 #'
 #' @export
-aa_get_metrics <- function(rsid = Sys.getenv("AA_RSID"),
+aa_get_metrics <- function(rsid = Sys.getenv("AA_REPORTSUITE_ID"),
                               locale = 'en_US',
                               segmentable = 'false',
-                              expansion = c('tags', 'allowedForReporting', 'categories')){
+                              expansion = 'tags, allowedForReporting, categories',
+                              company_id = Sys.getenv("AA_COMPANY_ID")){
+
+  #remove spaces from the lsit of expansion tags
+  expansion <- stringr::str_replace_all(expansion, fixed(" "), "")
   #create the url to send with the query
-
-  urlstructure <- sprintf(rsid,locale,segmentable)
-
-  #&segmentable=%s&reportable=%s&classifiable=%s
+  urlstructure <- sprintf("metrics?rsid=%s&locale=%s&segmentable=%s&expansion=%s",
+                                  rsid, locale, segmentable, expansion)
 
 
 
