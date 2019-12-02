2246288642032ed3c7253782649a56cd6241f895
Ben Woodard <benrwoodard@gmail.com>
2019-12-02 05:39:17 -0500

    Added a function (aa_workspace_report) which enables the end user to simply copy and paste the json string into the function and it returns the report in r.

---
+++ R/workspace_reports.R
@@ -0,0 +1,41 @@
+#' Use a prebuilt json query to pull a ranked report
+#'
+#' Organizes the arguments into a json string and then structures the data after the internal function makes
+#' the api call. Only runs a single dimension with as many metrics as you want.
+#'
+#' @param req_body The json string coppied from workspace
+#'
+#' @return Data Frame
+#'
+#' @export
+#'
+#'
+aa_workspace_report <- function(req_body) {
+
+  body <- fromJSON(req_body)
+
+  metrics <-  gsub(".*/", "", body$metricContainer$metrics$id)
+
+  dimensions <- gsub(".*/", "", body$dimension)
+
+  res <- aa_get_data("reports/ranked", body = body)
+
+  res <- fromJSON(res)
+
+  # Clean up and return as data frame
+  res_df <- res$rows
+
+  # If more than one metric the value list needs to be spread to individual columns
+  if(length(metrics) > 1 ) {
+     res_df <- res_df %>%
+                  unnest(data) %>%
+                  group_by(itemId,value) %>%
+                  mutate(col = seq_along(data)) %>%
+                  spread(key=col, value=data)
+  }
+
+  # Add column names to the dataset based on the metrics and dimensions
+  colnames(res_df) <- c(paste0(dimensions,'id'),dimensions,metrics)
+
+  res_df
+}
