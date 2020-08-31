ff8d43b908ce0dcd16cc10a5e175fa0b41006493
Ben Woodard <ben.woodard@searchdiscovery.com>
2020-08-30 20:26:09 -0400

    Adding an anomaly report

--- R/anomaly_report.R
+++ R/anomaly_report.R
@@ -1,22 +1,26 @@
-#' Get a timeseries reports
+#' Anomaly Report
+#'
+#' Get an anomaly report for one or more metrics
 #'
 #' @param rsid Adobe report number
-#' @param date_range A two length vector of start and end Date objects
+#' @param date_range A two length vector of start and end Date objects (default set to show last 30 days)
 #' @param metrics Metric to send
 #' @param pages number of report pages
-#' @param granularity use either minute, hour, day, week, month, quater, year
+#' @param granularity use either minute, hour, day, week, month, quarter, year
 #' @param sort either by 'desc' or 'asc' order
+#' @param anomalyDetection logical statement for including anomaly. Default is TRUE
 #'
 #'
 #' @export
 
-aa_rankedtime_report <- function(company_id = Sys.getenv('AA_COMPANY_ID'),
+aa_anomaly_report <- function(company_id = Sys.getenv('AA_COMPANY_ID'),
                                  rsid = Sys.getenv('AA_REPORTSUITE_ID'),
-                                 date_range,
+                                 date_range = c(Sys.Date()-31, Sys.Date()-1),
                                  metrics,
                                  pages = 0,
                                  granularity = 'day',
-                                 sort = 'desc'
+                                 sort = 'asc',
+                                 anomalyDetection = TRUE
                                  ){
 
 
@@ -49,10 +53,9 @@ aa_rankedtime_report <- function(company_id = Sys.getenv('AA_COMPANY_ID'),
                              settings = list(
                                dimensionSort = sort,
                                limit = limit,
-                               page = pages
-                             ),
-                             statistics = list(
-                               functions = c("col-max", "col-min")
+                               page = pages,
+                               nonesBehavior = "return-nones",
+                               includeAnomalyDetection = anomalyDetection
                              ) ) )
 
   res <- aa_call_data("reports/ranked", body = req_body,  company_id = company_id)
@@ -60,13 +63,25 @@ aa_rankedtime_report <- function(company_id = Sys.getenv('AA_COMPANY_ID'),
   res <- fromJSON(res)
 
   # Clean up and return as data frame
-  res_df <- res$rows %>%
-    unnest(data) %>%
-    group_by(itemId, value) %>%
-    mutate(col= seq_along(value))%>%
-    spread(key = col, value = data)
+  if(length(metrics) > 1) {
+    columnames <- colnames(res$rows[3:7])
+    res_df <- res$rows %>%
+      unnest(all_of(columnames)) %>%
+      group_by(itemId, value) %>%
+      mutate(metric = metrics) %>%
+      relocate(metric, .after = value) %>%
+      rename(date = value) %>%
+      mutate(date = as.Date(date, format = '%b %d, %Y'))
+  } else {
+    res_df <- res$rows %>%
+      mutate(metric = metrics) %>%
+      relocate(metric, .after = value) %>%
+      rename(date = value) %>%
+      mutate(date = as.Date(date, format = '%b %d, %Y'))
+  }
 
   # Add column names to the dataset based on the metrics and dimensions
+  res_df
   colnames(res_df) <- c('id',granularity,metrics)
 
   #res_df[2] <- as_date(res_df[2], tz = "UTC", format ="%b %d, %Y")
@@ -75,3 +90,4 @@ aa_rankedtime_report <- function(company_id = Sys.getenv('AA_COMPANY_ID'),
 
   df
 }
+
