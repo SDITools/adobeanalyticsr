f95917475bbcd843a6913484f780217e91d4625e
Ben Woodard <ben.woodard@Ben-Woodard2.local>
2020-10-27 09:32:22 -0400

    moved rowsdata back into the bigger freeform function and will work out a way to manage that function outside the main function for better package management going forward.  adding it to the enhancments list

--- R/freeform_report.R
+++ R/freeform_report.R
@@ -51,7 +51,7 @@ aa_freeform_report <- function(company_id = Sys.getenv("AA_COMPANY_ID"),
   }
 
   # 1 Call ------------------------------------------------------------------
-  for(i in seq(dimensions)) {
+for(i in seq(dimensions)) {
     if(i == 1) {
       finalnames <- c(dimensions, metrics)
 
@@ -331,10 +331,10 @@ aa_freeform_report <- function(company_id = Sys.getenv("AA_COMPANY_ID"),
 
       elnum <- sum(unlist(map(seq(length(res)), el)))
 
-      # rowsdata <- function(it) {
-      #   res[[it]]$rows %>% mutate(!!prefinalnames[[1]][[1]] := dat[[1]][[it]],
-      #                             !!prefinalnames[[1]][[2]] := dat[[2]][[it]])
-      # }
+      rowsdata <- function(it) {
+        res[[it]]$rows %>% mutate(!!prefinalnames[[1]][[1]] := dat[[1]][[it]],
+                                  !!prefinalnames[[1]][[2]] := dat[[2]][[it]])
+      }
 
       resrows <- map2_dfr(seq(elnum), i, rowsdata)
 
@@ -476,6 +476,126 @@ aa_freeform_report <- function(company_id = Sys.getenv("AA_COMPANY_ID"),
 
       #(ncapable)
       resn <- map(seq(length(res)),  getdata)
+
+    rowsdata <- function(it, i) {
+        if(i == 2) {
+          tf <- res[[it]]$rows %>% mutate(!!prefinalnames[[1]][[1]] := dat[[1]][[it]],
+                                          !!prefinalnames[[1]][[2]] := dat[[2]][[it]])
+          return(tf)
+        }
+        if(i == 3) {
+          tf <- resn[[it]]$rows %>% mutate(!!prefinalnames[[2]][[1]] := dat[[1]][it],
+                                           !!prefinalnames[[2]][[2]] := dat[[2]][it],
+                                           !!prefinalnames[[1]][[1]] := dat[[3]][it],
+                                           !!prefinalnames[[1]][[2]] := dat[[4]][it])
+          return(tf)
+        }
+        if(i == 4) {
+          tf <- resn[[it]]$rows %>% mutate(!!prefinalnames[[3]][[1]] := dat[[1]][it],
+                                           !!prefinalnames[[3]][[2]] := dat[[2]][it],
+                                           !!prefinalnames[[2]][[1]] := dat[[3]][it],
+                                           !!prefinalnames[[2]][[2]] := dat[[4]][it],
+                                           !!prefinalnames[[1]][[1]] := dat[[5]][it],
+                                           !!prefinalnames[[1]][[2]] := dat[[6]][it])
+          return(tf)
+        }
+        if(i == 5) {
+          tf <- resn[[it]]$rows %>% mutate(!!prefinalnames[[4]][[1]] := dat[[1]][it],
+                                           !!prefinalnames[[4]][[2]] := dat[[2]][it],
+                                           !!prefinalnames[[3]][[1]] := dat[[3]][it],
+                                           !!prefinalnames[[3]][[2]] := dat[[4]][it],
+                                           !!prefinalnames[[2]][[1]] := dat[[5]][it],
+                                           !!prefinalnames[[2]][[2]] := dat[[6]][it],
+                                           !!prefinalnames[[1]][[1]] := dat[[7]][it],
+                                           !!prefinalnames[[1]][[2]] := dat[[8]][it])
+          return(tf)
+        }
+        if(i == 6) {
+          tf <-  resn[[it]]$rows %>% mutate(!!prefinalnames[[5]][[1]] := dat[[1]][it],
+                                            !!prefinalnames[[5]][[2]] := dat[[2]][it],
+                                            !!prefinalnames[[4]][[1]] := dat[[3]][it],
+                                            !!prefinalnames[[4]][[2]] := dat[[4]][it],
+                                            !!prefinalnames[[3]][[1]] := dat[[5]][it],
+                                            !!prefinalnames[[3]][[2]] := dat[[6]][it],
+                                            !!prefinalnames[[2]][[1]] := dat[[7]][it],
+                                            !!prefinalnames[[2]][[2]] := dat[[8]][it],
+                                            !!prefinalnames[[1]][[1]] := dat[[9]][it],
+                                            !!prefinalnames[[1]][[2]] := dat[[10]][it])
+          return(tf)
+        }
+        if(i == 7) {
+          tf <- resn[[it]]$rows %>% mutate(!!prefinalnames[[6]][[1]] := dat[[1]][it],
+                                           !!prefinalnames[[6]][[2]] := dat[[2]][it],
+                                           !!prefinalnames[[5]][[1]] := dat[[3]][it],
+                                           !!prefinalnames[[5]][[2]] := dat[[4]][it],
+                                           !!prefinalnames[[4]][[1]] := dat[[5]][it],
+                                           !!prefinalnames[[4]][[2]] := dat[[6]][it],
+                                           !!prefinalnames[[3]][[1]] := dat[[7]][it],
+                                           !!prefinalnames[[3]][[2]] := dat[[8]][it],
+                                           !!prefinalnames[[2]][[1]] := dat[[9]][it],
+                                           !!prefinalnames[[2]][[2]] := dat[[10]][it],
+                                           !!prefinalnames[[1]][[1]] := dat[[11]][it],
+                                           !!prefinalnames[[1]][[2]] := dat[[12]][it])
+          return(tf)
+        }
+        if(i == 8) {
+          tf <- resn[[it]]$rows %>% mutate(!!prefinalnames[[7]][[1]] := dat[[1]][it],
+                                           !!prefinalnames[[7]][[2]] := dat[[2]][it],
+                                           !!prefinalnames[[6]][[1]] := dat[[3]][it],
+                                           !!prefinalnames[[6]][[2]] := dat[[4]][it],
+                                           !!prefinalnames[[5]][[1]] := dat[[5]][it],
+                                           !!prefinalnames[[5]][[2]] := dat[[6]][it],
+                                           !!prefinalnames[[4]][[1]] := dat[[7]][it],
+                                           !!prefinalnames[[4]][[2]] := dat[[8]][it],
+                                           !!prefinalnames[[3]][[1]] := dat[[9]][it],
+                                           !!prefinalnames[[3]][[2]] := dat[[10]][it],
+                                           !!prefinalnames[[2]][[1]] := dat[[11]][it],
+                                           !!prefinalnames[[2]][[2]] := dat[[12]][it],
+                                           !!prefinalnames[[1]][[1]] := dat[[13]][it],
+                                           !!prefinalnames[[1]][[2]] := dat[[14]][it])
+          return(tf)
+        }
+        if(i == 9) {
+          tf <- resn[[it]]$rows %>% mutate(!!prefinalnames[[8]][[1]] := dat[[1]][it],
+                                           !!prefinalnames[[8]][[2]] := dat[[2]][it],
+                                           !!prefinalnames[[7]][[1]] := dat[[3]][it],
+                                           !!prefinalnames[[7]][[2]] := dat[[4]][it],
+                                           !!prefinalnames[[6]][[1]] := dat[[5]][it],
+                                           !!prefinalnames[[6]][[2]] := dat[[6]][it],
+                                           !!prefinalnames[[5]][[1]] := dat[[7]][it],
+                                           !!prefinalnames[[5]][[2]] := dat[[8]][it],
+                                           !!prefinalnames[[4]][[1]] := dat[[9]][it],
+                                           !!prefinalnames[[4]][[2]] := dat[[10]][it],
+                                           !!prefinalnames[[3]][[1]] := dat[[11]][it],
+                                           !!prefinalnames[[3]][[2]] := dat[[12]][it],
+                                           !!prefinalnames[[2]][[1]] := dat[[13]][it],
+                                           !!prefinalnames[[2]][[2]] := dat[[14]][it],
+                                           !!prefinalnames[[1]][[1]] := dat[[15]][it],
+                                           !!prefinalnames[[1]][[2]] := dat[[16]][it])
+          return(tf)
+        }
+        if(i == 10) {
+          tf <- resn[[it]]$rows %>% mutate(!!prefinalnames[[9]][[1]] := dat[[1]][it],
+                                           !!prefinalnames[[9]][[2]] := dat[[2]][it],
+                                           !!prefinalnames[[8]][[1]] := dat[[3]][it],
+                                           !!prefinalnames[[8]][[2]] := dat[[4]][it],
+                                           !!prefinalnames[[7]][[1]] := dat[[5]][it],
+                                           !!prefinalnames[[7]][[2]] := dat[[6]][it],
+                                           !!prefinalnames[[6]][[1]] := dat[[7]][it],
+                                           !!prefinalnames[[6]][[2]] := dat[[8]][it],
+                                           !!prefinalnames[[5]][[1]] := dat[[9]][it],
+                                           !!prefinalnames[[5]][[2]] := dat[[10]][it],
+                                           !!prefinalnames[[4]][[1]] := dat[[11]][it],
+                                           !!prefinalnames[[4]][[2]] := dat[[12]][it],
+                                           !!prefinalnames[[3]][[1]] := dat[[13]][it],
+                                           !!prefinalnames[[3]][[2]] := dat[[14]][it],
+                                           !!prefinalnames[[2]][[1]] := dat[[15]][it],
+                                           !!prefinalnames[[2]][[2]] := dat[[16]][it],
+                                           !!prefinalnames[[1]][[1]] := dat[[17]][it],
+                                           !!prefinalnames[[1]][[2]] := dat[[18]][it])
+          return(tf)
+        }
+      }
 
       resrows <- map2_dfr(seq(length(resn)), i, rowsdata)
 
