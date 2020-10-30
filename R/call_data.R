0998313330e2775d2ffc5395be8ad87092f1b1e0
Ben Woodard <ben.woodard@searchdiscovery.com>
2020-10-29 21:21:46 -0400

    adding error handling for 206 errors in the api callfunction that will prevent all the data from pulling and the function will stop with an error.

--- R/call_data.R
+++ R/call_data.R
@@ -54,6 +54,13 @@ aa_call_data <- function(req_path,
                        `x-proxy-global-company-id` = company_id
                      ))
   stop_for_status(req)
+
+  if(status_code(req) == 206  & length(content(req)$columns$columnErrors[[1]]) != 0) {
+    stop(paste0('Pease check your metrics. You used an ',resrowserrors$columns$columnErrors$errorCode,' - ',resrowserrors$columns$columnErrors$errorDescription))
+  } else if(resrows$totalElements == 0){
+    stop(paste0('Please check dimension ', i, '. It did not pull back any data.'))
+  }
+
   httr::content(req, as = "text",encoding = "UTF-8")
 
 }
