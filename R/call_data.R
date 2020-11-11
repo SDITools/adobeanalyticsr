0e84f0de2666dca7f761af45123d215f1c096d55
Ben Woodard <ben.woodard@searchdiscovery.com>
2020-11-11 08:13:31 -0500

    revertting back to original 200 no data error handling

--- R/call_data.R
+++ R/call_data.R
@@ -58,8 +58,8 @@ aa_call_data <- function(req_path,
   } else if(status_code(req) == 206){
     stop(paste0('Please check the metrics your requested. A 206 error was returned.'))
 
-  } else if(status_code(req) == 200 & content(req)$totalElements != 0) {
-    httr::content(req, as = "text",encoding = "UTF-8")
-    #stop("There was a 200 response code (valid call) but no data was received. Check your attributes and try again.")
+  } else if(status_code(req) == 200 & content(req)$totalElements == 0) {
+    stop("There was a 200 response code (valid call) but no data was received. Check your attributes and try again.")
   }
+  httr::content(req, as = "text",encoding = "UTF-8")
 }
