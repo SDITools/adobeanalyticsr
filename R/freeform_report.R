b14f8720025fccfb82a02eaf361966dd82ff2014
Ben Woodard <ben.woodard@searchdiscovery.com>
2020-11-16 18:33:32 -0500

    using lubridate to parse date time element

--- R/freeform_report.R
+++ R/freeform_report.R
@@ -29,6 +29,7 @@
 #' @import purrrlyr
 #' @importFrom rlang :=
 #' @importFrom jsonlite fromJSON
+#' @importFrom lubridate parse_date_time
 #'
 #' @export
 aa_freeform_report <- function(company_id = Sys.getenv("AA_COMPANY_ID"),
@@ -503,10 +504,10 @@ for(i in seq(dimensions)) {
         dplyr::select(all_of(finalnames))
       #change time variables from character strings
       if("daterangeminute" %in% colnames(dat)) {
-        dat[names(dat) == 'daterangeminute'] <- as.POSIXlt(dat$daterangeminute, format = '%H:%M %Y-%m-%d', tz = 'GMT')
+        dat[names(dat) == 'daterangeminute'] <- lubridate::parse_date_time(dat$daterangeminute, orders = "ymd HMS")
       }
       if("daterangehour" %in% colnames(dat)) {
-        dat[names(dat) == 'daterangehour'] <- as.POSIXlt(dat$daterangehour, format = '%H:%M %Y-%m-%d', tz = 'GMT')
+        dat[names(dat) == 'daterangehour'] <- lubridate::parse_date_time(dat$daterangeminute, orders = "ymd HMS")
       }
       if("daterangeday" %in% colnames(dat)) {
         dat[names(dat) == 'daterangeday'] <- as.Date(dat$daterangeday, format = '%b %d, %Y')
