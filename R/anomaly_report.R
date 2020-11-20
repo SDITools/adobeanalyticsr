0698189cb24ce8a0cc9c57ba8681cb50afabaa90
Ben Woodard <ben.woodard@searchdiscovery.com>
2020-11-20 07:47:28 -0500

    change alpha to .02 to better see the actuals

--- R/anomaly_report.R
+++ R/anomaly_report.R
@@ -136,7 +136,7 @@ aw_anomaly_report <- function(company_id = Sys.getenv('AW_COMPANY_ID'),
           ggplot2::ggplot(aes(day, data)) +
           ggplot2::geom_line() +
           ggplot2::geom_point(data = dat %>% dplyr::filter(metric == metricname & dataAnomalyDetected == T), ggplot2::aes(day, data)) +
-          ggplot2::geom_ribbon(aes(ymin=dataLowerBound, ymax=dataUpperBound), alpha=0.4) +
+          ggplot2::geom_ribbon(aes(ymin=dataLowerBound, ymax=dataUpperBound), alpha=0.2) +
           ggplot2::labs(title = metricname,
                         subtitle = paste0('There are ',nrow(dat %>% filter(metric == metricname & dataAnomalyDetected == T)), ' anomalies.'),
                         caption =paste0('There are ',nrow(dat %>% filter(metric == metricname & dataAnomalyDetected == T)), ' anomalies.')) +
