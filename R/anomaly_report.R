58c33ec8fb2318886baea1b8d4172205c851a6ae
Ben Woodard <ben.woodard@searchdiscovery.com>
2020-11-19 22:59:49 -0500

    adding quickview option to anomaly detection report

--- R/anomaly_report.R
+++ R/anomaly_report.R
@@ -8,17 +8,22 @@
 #' @param metrics Metric to request the anomaly detection. If multiple metrics, each metric and date will have it's own row.
 #' @param segmentId Use segments to globally filter the results. Use 1 or many.
 #' @param granularity Use either hour, day (default), week, or month
+#' @param quickView Return a list of 3 lists per metric. 1. All Data 2. Data filtered to include only anomalous rows 3. Interactive ggplot line graph
 #' @param countRepeatInstances Should the data include repeat instances
 #' @param anomalyDetection logical statement for including anomaly. Default is TRUE
 #' @param debug default is FALSE but set to TRUE to see the json request being sent to the Adobe API
-#'
+#' @importFrom plotly ggplotly
+#' @importFrom scales comma
+#' @import ggplot2
 #' @export
+#'
 aw_anomaly_report <- function(company_id = Sys.getenv('AW_COMPANY_ID'),
                                  rsid = Sys.getenv('AW_REPORTSUITE_ID'),
                                  date_range = c(Sys.Date()-31, Sys.Date()-1),
                                  metrics,
                                  granularity = 'day',
                                  segmentId = NA,
+                                 quickView = FALSE,
                                  anomalyDetection = TRUE,
                                  countRepeatInstances = TRUE,
                                  debug = FALSE
@@ -119,7 +124,35 @@ aw_anomaly_report <- function(company_id = Sys.getenv('AW_COMPANY_ID'),
     }
 
     message(paste0('A total of ',nrow(dat), ' rows have been pulled.'))
-
+  if(quickView == F) {
     return(dat)
+  }
+    if(quickView == T) {
+      quickview <- function(metricname) {
+        table <- dat %>% dplyr::filter(metric == metricname | dataAnomalyDetected == T)
+
+        graph <- dat %>%
+          dplyr::filter(metric == metricname) %>%
+          ggplot2::ggplot(aes(day, data)) +
+          ggplot2::geom_line() +
+          ggplot2::geom_point(data = dat %>% dplyr::filter(dataAnomalyDetected == T), ggplot2::aes(day, data)) +
+          ggplot2::geom_ribbon(aes(ymin=dataLowerBound, ymax=dataUpperBound), alpha=0.4) +
+          ggplot2::labs(title = eventnm,
+                        subtitle = paste0('There are ',nrow(dat %>% filter(dataAnomalyDetected == T)), ' anomalies.'),
+                        caption =paste0('There are ',nrow(dat %>% filter(dataAnomalyDetected == T)), ' anomalies.')) +
+          ggplot2::theme_bw() +
+          ggplot2::theme(axis.title.x = element_blank(), axis.title.y = element_blank(), legend.position = 'none') +
+          ggplot2::scale_y_continuous(labels = scales::comma) +
+          ggplot2::expand_limits(y=0)
+
+        list(data = dat, anoms = table, viz = plotly::ggplotly(graph))
+        }
+        qv <- purrr::map(metrics, qickview)
+
+        return(qv)
+      }
+
+
+
 }
 
