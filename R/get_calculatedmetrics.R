b7aa355cb535e0887832e277ad74ae59f2bab952
Ben Woodard <ben.woodard@searchdiscovery.com>
2020-11-30 19:21:02 -0500

    updating tagNames

--- R/get_calculatedmetrics.R
+++ R/get_calculatedmetrics.R
@@ -9,7 +9,7 @@
 #' @param toBeUsedInRsid The report suite where the calculated metric intended to be used. This report suite will be used to determine things like compatibility and permissions. If it is not specified then the permissions will be calculated based on the union of all metrics authorized in all groups the user belongs to. If the compatibility expansion is specified and toBeUsedInRsid is not then the compatibility returned is based off the compatibility from the last time the calculated metric was saved.
 #' @param locale Locale (en_US default)
 #' @param name Filter list to only include calculated metrics that contains the Name
-#' @param tagnames Filter list to only include calculated metrics that contains one of the tags
+#' @param tagNames Filter list to only include calculated metrics that contains one of the tags
 #' @param favorite Filter list to only include calculated metrics that are favorites (boolean)
 #' @param approved Filter list to only include calculated metrics that are approved (boolean)
 #' @param limit Number of results per page
@@ -18,6 +18,7 @@
 #' @param sortProperty Property to sort by (name, modified_date, id is currently allowed)
 #' @param expansion Comma-delimited list of additional calculated metric metadata fields to include on response. (options = reportSuiteName, ownerFullName, modified, tags, definition, compatability, categories)
 #' @param includeType Include additional calculated metrics not owned by user. The "all" option takes precedence over "shared" (options = all, shared, templates)
+#' @param debug
 #'
 #' @import stringr
 #' @export
@@ -29,7 +30,7 @@ aw_get_calculatedmetrics <- function(company_id = Sys.getenv("AW_COMPANY_ID"),
                           toBeUsedInRsid = NA,
                           locale = "en_US",
                           name = NA,
-                          tagnames = NA,
+                          tagNames = NA,
                           favorite = NA,
                           approved = NA,
                           limit = 1000,
@@ -37,25 +38,30 @@ aw_get_calculatedmetrics <- function(company_id = Sys.getenv("AW_COMPANY_ID"),
                           sortDirection = 'DESC',
                           sortProperty = NA,
                           expansion = NA,
-                          includeType = 'all')
+                          includeType = 'all',
+                          debug = FALSE)
 {
-  #make the list of params into a dataframe
-  if(length(rsids) > 1) {rsids = paste0(rsids, collapse = ',') }
+  #edit the character vectors to the string they need to be
+  if(length(rsids) > 1) {rsids = URLencode(paste0(rsids, collapse = ',')) }
   if(length(filterByIds) > 1) {filterByIds = paste0(filterByIds, collapse = ',') }
-  if(length(expansion) > 1) {expansion = paste0(expansion, collapse = ',') }
+  if(!is.na(tagnames)) {tagnames = URLencode(paste0(tagnames, collapse = ',')) }
+  if(length(expansion) > 1) {expansion = paste(expansion, collapse = ',', sep = '') }
+
+  #includeType is case senstative
+  includeType <- tolower(includeType)
 
   vars <- tibble::tibble(rsids, ownerId, filterByIds, toBeUsedInRsid, locale, name, tagnames, favorite, approved,
-  limit, page, sortDirection, sortProperty, expansion, includeType)
+                         limit, page, sortDirection, sortProperty, expansion, includeType)
   #Turn the list into a string to create the query
-  prequery <- list(vars %>% select_if(~ !any(is.na(.))))
+  prequery <- vars %>% select_if(~ !any(is.na(.)))
   #remove the extra parts of the string and replace it with the query parameter breaks
-  query_param <- stringr::str_remove_all(str_replace_all(str_remove_all(paste(prequery, collapse = ''), '\\"'), ', ', '&'), 'list\\(| |\\)')
+  query_param <-  paste(names(prequery), prequery, sep = '=', collapse = '&')
 
   #create the url to send with the query
   urlstructure <- paste0('calculatedmetrics?',query_param)
 
   #urlstructure <- 'segments?locale=en_US&filterByPublishedSegments=all&limit=1000&page=0&sortDirection=ASC&sortProperty=id&includeType=all'
-  res <- aw_call_api(req_path = urlstructure[1], company_id = company_id)
+  res <- aw_call_api(req_path = urlstructure[1], debug = debug, company_id = company_id)
 
   res <- jsonlite::fromJSON(res)
 
