a71ef5cb256dcf3871071e6fb64168f964c2a836
Ben Woodard <ben.woodard@searchdiscovery.com>
2021-02-17 07:35:45 -0500

    add functiong get_usage() based on new endpoint add glue::glue function instead of paste0 in make_timeframe() function and add support for startdate and enddate list items returned for the new endpoint. Since utilities_function uses the same 2 arguments this was a quick addition and will help maintenance in the future

---
+++ R/get_usage.R
@@ -0,0 +1,62 @@
+#' Get a list of user usage
+#'
+#' This function returns the usage and access logs for a given date range within a 3 month period. This API authenticates with an IMS user token..
+#'
+#' @param startDate Start date for the maximum of a 3 month period.
+#' @param endDate End date for the maximum of a 3 month period.
+#' @param login The login value of the user you want to filter logs by.
+#' @param ip The IP address you want to filter logs by.
+#' @param rsid The report suite ID you want to filter logs by.
+#' @param eventType The numeric id for the event type you want to filter logs by.
+#' @param event The event description you want to filter logs by. No wildcards permitted.
+#' @param limit The number of results to return per page. This argument works in conjunction with the
+#' `page` argument. The default is 10.
+#' @param page The "page" of results to display. This works in conjunction with the `limit` argument and is
+#' zero-based. For instance, if `limit = 20` and `page = 1`, the results returned would be 21 through 40.
+#' @param company_id Company ID. If an environment variable called `AW_COMPANY_ID` exists in `.Renviron` or
+#' elsewhere and no `company_id` argument is provided, then the `AW_COMPANY_ID` value will be used.
+#' Use \code{\link{get_me}} to get a list of available `company_id` values.
+#'
+#' @return A data frame of users and their meta data.
+#' @examples
+#' \dontrun{
+#' get_usage(startDate = Sys.Date()-91, endDate = Sys.Date()-1, limit = 100, page = 0)
+#' }
+#'
+#' @import stringr
+#' @export
+#'
+get_usage <- function(startDate = Sys.Date()-91,
+                      endDate = Sys.Date()-1,
+                      login = NA,
+                      ip = NA ,
+                      rsid = NA,
+                      eventType = NA,
+                      event = NA,
+                      limit = 100,
+                      page = 0,
+                      company_id = Sys.getenv("AW_COMPANY_ID")
+                      )
+{
+
+  vars <- tibble::tibble(login, ip, rsid, eventType, event, limit, page)
+  #Turn the list into a string to create the query
+  prequery <- list(dplyr::select_if(vars, ~ !any(is.na(.))))
+  #remove the extra parts of the string and replace it with the query parameter breaks
+  query_param <- stringr::str_remove_all(stringr::str_replace_all(stringr::str_remove_all(paste(prequery, collapse = ''), '\\"'), ', ', '&'), 'list\\(| |\\)')
+
+  #create the url to send with the query
+  urlstructure <- paste0('users?',query_param)
+
+  #urlstructure <- 'segments?locale=en_US&filterByPublishedSegments=all&limit=1000&page=0&sortDirection=ASC&sortProperty=id&includeType=all'
+  res <- aw_call_api(req_path = urlstructure[1], company_id = company_id)
+
+  res <- jsonlite::fromJSON(res)
+
+  #Just need the content of the returned json
+  res <- res$content
+
+  res
+
+}
+
--- R/utilities_function.R
+++ R/utilities_function.R
@@ -12,5 +12,13 @@ make_timeframe <- function(start_date, end_date){
   if(!grepl('-', end_date)) {
     end_date<- as.Date(as.numeric(end_date),origin = "1970-01-01")
   }
-  paste0(start_date, "T00:00:00.000/", end_date, "T23:59:59.999")
+  glue::glue('{start_date}T00:00:00.000/{end_date}T23:59:59.999')
+}
+
+#set the timeframe for the query (timeframe)
+make_startDate_endDate <- function(start_date, end_date){
+  if(!grepl('-', start_date)) {
+    end_date<- as.Date(as.numeric(end_date),origin = "1970-01-01")
+  }
+  list(glue::glue('{start_date}T00:00:00.000'), glue::glue('{end_date}T23:59:59.999'))
 }
