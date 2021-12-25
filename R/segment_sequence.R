5ecb5ec401b7e46d0e438e154a9f11439d3215cd
Ben Woodard <ben.woodard@searchdiscovery.com>
2021-12-25 02:09:53 -0500

    fix exclude_checkpoint function and add validation for arguments

--- R/segment_sequence.R
+++ R/segment_sequence.R
@@ -6,7 +6,7 @@
 #' @param predicates List of predicates created using `seg_pred()` function. Must wrapped in a list() function.
 #' @param sequence  How should the sequence of items be considered. Options: `in_order` (default), `before`, `after`, `and`, `or`
 #' @param exclude Excludes the entire sequence container which will include all predicates.
-#' @param exclude_checkpoint Which checkpoints (predicates) should be excluded.
+#' @param exclude_checkpoint Which checkpoints (predicates) should be excluded. Example `c(1, 4)`
 #'
 #' @details
 #'
@@ -30,6 +30,24 @@ seg_seq <- function(context = 'visits',
                     exclude = FALSE,
                     exclude_checkpoint = NULL) {
 
+#validate 'sequence' argument value
+  if(!sequence %in% c('in_order', 'after', 'before', 'and', 'or')){
+   stop("Make sure the `sequence` argument is either 'in_order' (default), 'after', 'before', 'and', or 'or'.")
+  }
+#validate 'context' argument value
+  if(!context %in% c('hits', 'visits', 'visitors')){
+    stop("Make sure the `context` argument is either 'hits', 'visits' (default), or 'visitors'")
+  }
+#validate 'exclude' argument value
+  if(!is.logical(exclude)){
+    stop("The argument `exclude` must be either True or False.")
+  }
+#validate 'exclude_checkpoint' argument value
+  if(!is.null(exclude_checkpoint) & !is.numeric(exclude_checkpoint)){
+    stop("The argument 'exclude_checkpoint' needs to be NULL, a single number, or a vector of numbers defining the predicates you want to exclude.")
+  }
+
+
 sequence_dir <- dplyr::case_when(sequence == 'in_order' ~ 'sequence',
                                  sequence == 'after' ~ 'sequence-prefix',
                                  sequence == 'before' ~ 'sequence-suffix',
@@ -38,12 +56,11 @@ sequence_dir <- dplyr::case_when(sequence == 'in_order' ~ 'sequence',
 
 ## Append the exclusion list item
 if(!is.null(exclude_checkpoint)) {
-  for(i in seq(exclude_checkpoint)) {
-    exchecks <- seq(exclude_checkpoint)-1
-  predicates <- append(predicates,
-                       list(list(func = 'exclude-next-checkpoint')),
-                       exclude_checkpoint[[i]]+exchecks[[i]]-1
-                       )
+  new_checkpoints <- (exclude_checkpoint-1) + (seq(length(exclude_checkpoint)) - 1)
+  for(i in new_checkpoints) {
+    predicates <- append(predicates,
+                         list(list(func = 'exclude-next-checkpoint')),
+                         after = i)
   }
 }
 ## Add in the necessary 'container' and 'hits' variables to each predicate for sequence to work
