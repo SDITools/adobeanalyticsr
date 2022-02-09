992a487497e90ef8909df01e1f5547e6f01640aa
benrwoodard <ben.woodard@searchdiscovery.com>
2022-02-09 11:19:13 -0500

    add check for unit name validation, increased functionals included for easier code readability, bug squashing, and enhancement ability

--- R/segment_then.R
+++ R/segment_then.R
@@ -31,77 +31,90 @@
 #'
 seg_then <- function(limit = 'within',
                      count =  1,
-                     unit = 'week') {
+                     unit = 'year') {
 #validation check for the limit argument
   #Check the spelling of the 2 variables
-  for (i in 1:length(limit)){
-    if (!is.element(limit[i], c('within', 'after'))){
+  for (i in 1:length(limit)) {
+    if (!is.element(limit[i], c('within', 'after'))) {
       stop ("Make sure you have spelled `after` or `within` correctly.")
     }
   }
   #check that the count is a numeric
   assertthat::assert_that(is.numeric(count))
 
-  if (length(limit) > 2 | length(count) > 2 | length(unit) > 2) {
+  if (length(limit) > 2 || length(count) > 2 || length(unit) > 2) {
     stop("Only 2 rules can be combined in a single sequence time restriction.")
   }
-  if (length(limit) == 2 & sum(duplicated(limit)) == 1) {
-    stop("The limit values cannot be the same. Use either ")
+  if (length(limit) == 2 && sum(duplicated(limit)) == 1) {
+    stop("The limit values cannot be the same. Use either 'within' and 'after' or only one.")
   }
+  unit_options <- c('hit', 'visit', 'minute', 'hour', 'day', 'week', 'month', 'quarter', 'year')
+  unit_name_test <- function(x) {
+      if (!x %in% unit_options) {
+        stop("The unit argument must be either: 'hit', 'visit', 'minute', 'hour', 'day', 'week', 'month', 'quarter', 'year'")
+      } else { glue::glue("unit '{x}' is good")}
+  }
+  map(unit, unit_name_test)
 
   if (length(limit) == 2 && which(limit == 'within') == 2) {
     limit <- rev(limit)
     count <- rev(count)
     unit <- rev(unit)
   }
+  #function to create the json if both after and within are used
+  both_then <- function(count, limit, unit) {
+    restriction <- list(
+      list(count = count[[1]],
+           limit = limit[[1]],
+           container = 'hits',
+           unit = unit[[1]],
+           func = 'time-restriction'),
+      list(count = count[[2]],
+           limit = limit[[2]],
+           container = 'hits',
+           unit = unit[[2]],
+           func = 'time-restriction')
+    )
 
-if(length(limit) == 2){
-  restriction <- list(
-       list(count = count[[1]],
-             limit = limit[[1]],
-             container = 'hits',
-             unit = unit[[1]],
-             func = 'time-restriction'),
-        list(count = count[[2]],
-             limit = limit[[2]],
-             container = 'hits',
-             unit = unit[[2]],
-             func = 'time-restriction')
-  )
-  #check where units == 'hit'
-  for(i in 1:2){
-    if(length(restriction[[i]]$unit) > 0 && restriction[[i]]$unit == 'hit'){
-      restriction[[i]]['unit'] <- NULL
-      restriction[[i]]['func'] <- 'container-restriction'
+    #check where units == 'hit'
+    for (i in 1:2) {
+      if (length(restriction[[i]]$unit) > 0 && restriction[[i]]$unit == 'hit') {
+        restriction[[i]]['unit'] <- NULL
+        restriction[[i]]['func'] <- 'container-restriction'
+      }
+      if (!is.null(restriction[[i]]$unit) && restriction[[i]]$unit == 'visit') {
+        restriction[[i]]['container'] <- 'visits'
+        restriction[[i]]['unit'] <- NULL
+        restriction[[i]]['func'] <- 'container-restriction'
+      }
     }
-    if(!is.null(restriction[[i]]$unit) && restriction[[i]]$unit == 'visit'){
-      restriction[[i]]['container'] <- 'visits'
-      restriction[[i]]['unit'] <- NULL
-      restriction[[i]]['func'] <- 'container-restriction'
-    }
+    restriction
   }
-
+  single_then <- function(count, limit, unit) {
+    if (unit[[1]] == 'hit') {
+      restriction <- list(count = count[[1]],
+                          limit = limit[[1]],
+                          container = 'hits',
+                          func = 'container-restriction')
+    } else if (unit[[1]] == 'visit') {
+      restriction <- list(count = count[[1]],
+                          limit = limit[[1]],
+                          container = 'visits',
+                          func = 'container-restriction')
+    } else {
+      restriction <- list(count = count[[1]],
+                          limit = limit[[1]],
+                          container = 'hits',
+                          unit = unit[[1]],
+                          func = 'time-restriction')
+    }
+    restriction
+  }
+if (length(limit) == 2) {
+  both_then(count, limit, unit)
 } else {
-  if(unit[[1]] == 'hit'){
-  restriction <- list(count = count[[1]],
-                      limit = limit[[1]],
-                      container = 'hits',
-                      func = 'container-restriction')
-  } else if(unit[[1]] == 'visit'){
-    restriction <- list(count = count[[1]],
-                        limit = limit[[1]],
-                        container = 'visits',
-                        func = 'container-restriction')
-  } else {
-  restriction <- list(count = count[[1]],
-                      limit = limit[[1]],
-                      container = 'hits',
-                      unit = unit[[1]],
-                      func = 'time-restriction')
-  }
+  single_then(count, limit, unit)
 }
-
-return(restriction)
 
 }
 
